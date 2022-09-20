98989be2ece4046d21e33382c4051f70e14a9537
dylanndo <dylan2002do@yahoo.com>
2022-09-19 22:52:09 -0400

    create main file to run analysis

---
+++ main.py
@@ -0,0 +1,15 @@
+players = {}
+# extract data from shots_data.csv
+with open('shots_data.csv', 'r') as file:
+    csvreader = csv.reader(file)
+    header = next(csvreader)
+    for row in csvreader:
+        if row[0] in players:
+            kwopkepd
+        else:
+            players[row[0]] = 
+        if(row[0] ==)
+        if(row[0] == "Bradley Beal"): # organize by Player
+            teamA.addShot(row)
+        elif(row[0] == "John Wall"):    
+            teamB.addShot(row)
