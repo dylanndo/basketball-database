419cfb7886bd5d0200742271ab3eb3a8be4b08b4
dylanndo <dylan2002do@yahoo.com>
2022-09-21 16:31:43 -0400

    incorporate way to read csv file

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-players = {}
+import csv
+from player import Player
+
+players = {}    # dictionary of playerName:Player()
+
 # extract data from shots_data.csv
 with open('shots_data.csv', 'r') as file:
     csvreader = csv.reader(file)
     header = next(csvreader)
     for row in csvreader:
-        if row[0] in players:
-            kwopkepd
-        else:
-            players[row[0]] = 
-        if(row[0] ==)
-        if(row[0] == "Bradley Beal"): # organize by Player
-            teamA.addShot(row)
-        elif(row[0] == "John Wall"):    
-            teamB.addShot(row)
+        if row[0] in players:   #if player is in database already
+            players[row[0]].addShot(row)
+        else:   #otherwise add player first then add stats
+            globals()[row[0]] = Player()
+            players[row[0]] = row[0]
+            players[row[0]].addShot(row)
+
