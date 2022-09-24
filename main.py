f205cb14b1ffa04cc2d145243d2811f4c7b8b764
dylanndo <dylan2002do@yahoo.com>
2022-09-23 22:55:33 -0400

    make players a dictionary of player objects

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import csv
 from player import Player
 
-players = {}    # dictionary of playerName:Player()
+players = {}    # dictionary of Player objects
 
 # extract data from shots_data.csv
 with open('shots_data.csv', 'r') as file:
     csvreader = csv.reader(file)
     header = next(csvreader)
     for row in csvreader:
-        if row[0] in players:   #if player is in database already
-            players[row[0]].addShot(row)
+        name = row[0]
+        if name in players:   #if player is in database already
+            players.get(name).addShot(row)
         else:   #otherwise add player first then add stats
-            globals()[row[0]] = Player()
-            players[row[0]] = row[0]
-            players[row[0]].addShot(row)
+            globals()[name] = Player()
+            players[name] = (globals()[name])
+            players.get(name).addShot(row)
 
+for x in players:
+    print("Stats for " + players.get(x) + ":")
+    players.get(x).printStats()
