9d5a0c3fa204f12be6a691cb2147797a16d6060e
dylanndo <dylan2002do@yahoo.com>
2022-09-25 23:29:03 -0400

    import Menu(), incorporate Menu() functions

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import csv
 from player import Player
+from menu import Menu
 
 players = {}    # dictionary of Player objects
+userInput = ""  # user's input/choice
+menuOption = 0
 
 # extract data from shots_data.csv
 with open('shots_data.csv', 'r') as file:
@@ -16,7 +19,70 @@ with open('shots_data.csv', 'r') as file:
             players[name] = (globals()[name])
             players.get(name).addShot(row)
 
-for x in players:
-    print("Stats for " + x + ":")
-    players.get(x).printStats()
-    print()
+while(userInput.lower() != "done" and menuOption != 6):
+    userInput = input("Who's statistics would you like to access? (Type 'done' to exit) ")
+
+    if userInput in players:
+        menuOption = Menu.mainMenu()
+
+        if menuOption == 1:
+            menuOption = Menu.zoneMenu1()
+
+            if menuOption == 1:
+                Menu.printFGp2PT(players[userInput])
+            elif menuOption == 2:
+                Menu.printFGp3PT(players[userInput])
+            elif menuOption == 3:
+                Menu.printFGpC3(players[userInput])
+            elif menuOption == 4:
+                Menu.printFGpNC3(players[userInput])
+            elif menuOption == 5:
+                Menu.printFGp(players[userInput])
+            elif menuOption == 6:
+                Menu.printAllFGp(players[userInput])
+        
+        elif menuOption == 2:
+            menuOption = Menu.zoneMenu2()
+
+            if menuOption == 1:
+                Menu.printZone2PT(players[userInput])
+            elif menuOption == 2:
+                Menu.printZone3PT(players[userInput])
+            elif menuOption == 3:
+                Menu.printZoneC3(players[userInput])
+            elif menuOption == 4:
+                Menu.printZoneNC3(players[userInput])
+            elif menuOption == 5:
+                Menu.printAllZones(players[userInput])
+
+        elif menuOption == 3:
+            menuOption = Menu.zoneMenu1()
+
+            if menuOption == 1:
+                Menu.printEFGp2PT(players[userInput])
+            elif menuOption == 2:
+                Menu.printEFGp3PT(players[userInput])
+            elif menuOption == 3:
+                Menu.printEFGpC3(players[userInput])
+            elif menuOption == 4:
+                Menu.printEFGpNC3(players[userInput])
+            elif menuOption == 5:
+                Menu.printEFGp(players[userInput])
+            elif menuOption == 6:
+                Menu.printAllEFGp(players[userInput])
+
+        elif menuOption == 4:
+            Menu.printAllFGp(players[userInput])
+            print()
+            Menu.printAllZones(players[userInput])
+            print()
+            Menu.printAllEFGp(players[userInput])
+
+        elif menuOption == 5:
+            break            
+            
+    else:
+        print("This player is not in the database. Enter anything to continue ")
+    
+    menuOption = 0
+
