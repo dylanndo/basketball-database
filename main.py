913c605cd4744b156ac96349c29b867d1ccaf000
dylanndo <dylan2002do@yahoo.com>
2022-09-26 00:56:43 -0400

    add quit and bypass bools + quit feature

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ from menu import Menu
 
 players = {}    # dictionary of Player objects
 userInput = ""  # user's input/choice
+mainMenuOption = 0
 menuOption = 0
+quit = False    # true if user wants to quit program
+bypass = False  # true in the instance that user wants to choose a new player, skips main menu
 
 # extract data from shots_data.csv
 with open('shots_data.csv', 'r') as file:
     csvreader = csv.reader(file)
     header = next(csvreader)
     for row in csvreader:
-        name = row[0].lower()
+        name = row[0].upper()
         if name in players:   #if player is in database already
             players.get(name).addShot(row)
         else:   #otherwise add player first then add stats
@@ -19,20 +22,34 @@ with open('shots_data.csv', 'r') as file:
             players[name] = (globals()[name])
             players.get(name).addShot(row)
 
-while(menuOption != -1):
+while(quit == False):   # while user hasn't quit
     print()
-    menuOption = Menu.mainMenu()
-
-    if menuOption == 1:
-        userInput = input("Who's statistics would you like to access? (Type 'done' to exit): ").lower()
+    if bypass == False: # if user hasn't chosen to select a new player, get a main menu option
+        mainMenuOption = int(float(Menu.mainMenu()))
         print()
-        
-        if userInput in players:
-            menuOption = Menu.mainMenu()
+
+    if mainMenuOption == 1: # ask for which player to access
+        userInput = input("Who's statistics would you like to access? (Type 'done' to exit): ").upper()
+        print()
+
+        if(userInput.lower() == "done"):
+            quit = True
+
+        else:
+            while not userInput in players:    # while user has input an invalid player
+                print("This player is not in the database.")
+                print()
+                userInput = input("Who's statistics would you like to access? (Type 'done' to exit): ").upper()
+                print()
+                if(userInput.lower() == "done"):
+                    quit = True
+
+
+            menuOption = int(float(Menu.playerStatsScreen()))
             print()
 
             if menuOption == 1:
-                menuOption = Menu.zoneMenu1()
+                menuOption = int(float(Menu.zoneMenu1()))
                 print()
 
                 if menuOption == 1:
@@ -49,7 +66,7 @@ while(menuOption != -1):
                     Menu.printAllFGp(players[userInput])
             
             elif menuOption == 2:
-                menuOption = Menu.zoneMenu2()
+                menuOption = int(float(Menu.zoneMenu2()))
                 print()
 
                 if menuOption == 1:
@@ -64,7 +81,7 @@ while(menuOption != -1):
                     Menu.printAllZones(players[userInput])
 
             elif menuOption == 3:
-                menuOption = Menu.zoneMenu1()
+                menuOption = int(float(Menu.zoneMenu1()))
                 print()
 
                 if menuOption == 1:
@@ -91,21 +108,16 @@ while(menuOption != -1):
                 print()
 
             elif menuOption == 5:
-                print()
+                bypass = True
                 
             elif menuOption == 6:
-                menuOption = -1
-                
-        elif userInput.lower() == "done":
-            menuOption = -1
+                bypass = False
 
-        else:
-            print("This player is not in the database.")
-            print()
-        
-        menuOption = 0
+            elif menuOption == 7:
+                quit = True
+            
 
-    elif menuOption == 2:
+    elif mainMenuOption == 2:
         print()
         print("Here is a list of the players in our current database:")
         
@@ -113,5 +125,5 @@ while(menuOption != -1):
             print(x)
         
         print()
-    elif menuOption == 3:
-        menuOption == -1
+    elif mainMenuOption == 3:
+        quit = True
