4d7e1aeda56a9aab8eac1e97384081731ec475ef
dylanndo <dylan2002do@yahoo.com>
2022-09-25 21:54:29 -0400

    add updateEGP, updateFGP

--- stats_calculator.py
+++ stats_calculator.py
@@ -10,3 +10,11 @@ class Stats_Calculator():
     # calculates effective field goal percentage in given 3 pt zone
     def update3EFGP(madeShots, attemptedShots):
         return (1.5 * madeShots) / attemptedShots
+    
+    # calculates total effective field goal percentage
+    def updateEFGP(madeShots, made3PT, attemptedShots):
+        return (madeShots + 1.5 * made3PT) / attemptedShots
+
+    # calculates field goal percentage
+    def updateFGP(madeShots, attemptedShots):
+        return madeShots / attemptedShots
